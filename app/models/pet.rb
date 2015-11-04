cc43830b63bd86224afb3f3136cdd4c812f97145
rrrepsaj <thejman222@gmail.com>
2015-11-04 20:42:17 +0000

    Add pet validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,13 @@
 class Pet < ActiveRecord::Base
   belongs_to :user
   
+  PET_TYPES = %w(Dog Cat Bird)
+  
+  validates :name_of_pet, :presence => true, :length => { maximum: 35 }
+  validates_inclusion_of :type_of_pet, in: PET_TYPES
+  validates :breed, :presence => true, :length => { maximum: 35 }
+  validates :age, :presence => true
+  validates :weight, :presence => true
+  validates :date_last_visited, :presence => true
+  
 end
