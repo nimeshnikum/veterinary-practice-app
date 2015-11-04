0fe14ecf41f53ecf31b9c137616510d9365621d4
rrrepsaj <thejman222@gmail.com>
2015-11-04 21:26:52 +0000

    Add Appointment validations

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,16 @@ class Appointment < ActiveRecord::Base
   belongs_to :user
   belongs_to :pet
   
+  validates :date_of_visit, :presence => true
+  validate :not_in_the_past
+  validates :pet_id, :presence => true
+  validates :user_id, :presence => true
+  validates :reason_for_visit, :presence => true
+  
+  def not_in_the_past
+    if date_of_visit < Date.today
+      errors.add(:date, "cannot schedule a date in the past.")
+    end
+  end
+  
 end
